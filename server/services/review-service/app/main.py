179836adf5da1c7a43e0c05e4265761a5853c3b7
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2026-04-27 21:50:08 +0530

    feat: initialize FastAPI application with CORS middleware and database setup

--- server/services/review-service/app/main.py
+++ server/services/review-service/app/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+from app.db.db import engine, base
+from app.model.review import Comment
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="http://localhost:5173",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.on_event("startup")
+async def on_startup():
+    print("Creating database tables...")
+    base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def read_root():
