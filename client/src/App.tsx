6ca63a4d7e90f91f9a52eea63c3e73f6c22b40eb
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2026-07-14 13:15:46 +0530

    feat: add ScrollRestoration to App component for improved navigation experience

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import { Toaster } from "./components/ui/sonner";
 
 export default function App() {
@@ -7,6 +7,7 @@ export default function App() {
     <div>
       <Toaster />
       <Outlet />
+      <ScrollRestoration />
     </div>
   );
 }
